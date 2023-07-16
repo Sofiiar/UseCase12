f2020bae8331d2de67210179aad4f3757eafa0ec
Sofiiar <s_radul-itssv@kw.com>
2023-07-16 19:44:25 +0200

    UC#12 Create documentation

--- CommentEndpoint.java
+++ CommentEndpoint.java
@@ -1,3 +1,14 @@
+/**
+ * Represents a REST API endpoint for managing comment resources.
+ * This class extends the AbstractWebEndpoint class.
+ *
+ * <p>
+ * The CommentEndpoint class provides methods for creating, updating, and retrieving comment information
+ * via the REST API. It inherits common functionality from the AbstractWebEndpoint class.
+ * </p>
+ *
+ * @see AbstractWebEndpoint
+ */
 package com.softserve.taf.services.placeholder.endpoints;
 
 import io.restassured.response.ValidatableResponse;
@@ -15,63 +26,164 @@ public class CommentEndpoint extends AbstractWebEndpoint {
     private static final String COMMENTS_END = "/comments";
     private static final String COMMENTS_RESOURCE_END = "/comments/{commentID}";
 
+    /**
+     * Constructs a new CommentEndpoint with the given RequestSpecification.
+     *
+     * @param specification the RequestSpecification used for API requests
+     * @see RequestSpecification
+     */
     public CommentEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+     * Creates a new comment using the provided CommentDto object.
+     *
+     * @param commentDto the CommentDto object representing the comment to be created
+     * @return the created CommentDto object
+     * @see CommentDto
+     */
     public CommentDto create(CommentDto commentDto) {
         return create(commentDto, HttpStatus.CREATED)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Creates a new comment using the provided CommentDto object and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param commentDto the CommentDto object representing the comment to be created
+     * @param status     the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see CommentDto
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * CommentDto comment = new CommentDto("Hello, nice post!");
+     * CommentEndpoint commentEndpoint = new CommentEndpoint(requestSpecification);
+     * CommentDto createdComment = commentEndpoint.create(comment);
+     * System.out.println("Created Comment ID: " + createdComment.getId());
+     * }</pre>
+     */
     public ValidatableResponse create(CommentDto commentDto, HttpStatus status) {
         LOGGER.info("Create new Comment");
         return post(
-            this.specification,
-            COMMENTS_END,
-            commentDto)
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_END,
+                commentDto)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Updates an existing comment with the provided CommentDto object and comment ID.
+     *
+     * @param id         the ID of the comment to be updated
+     * @param commentDto the CommentDto object representing the updated comment
+     * @return the updated CommentDto object
+     * @see CommentDto
+     */
     public CommentDto update(int id, CommentDto commentDto) {
         return update(commentDto, id, HttpStatus.OK)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Updates an existing comment with the provided CommentDto object, comment ID, and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param commentDto the CommentDto object representing the updated comment
+     * @param id         the ID of the comment to be updated
+     * @param status     the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see CommentDto
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * CommentDto comment = new CommentDto("Updated comment");
+     * CommentEndpoint commentEndpoint = new CommentEndpoint(requestSpecification);
+     * CommentDto updatedComment = commentEndpoint.update(1, comment);
+     * System.out.println("Updated Comment: " + updatedComment.getContent());
+     * }</pre>
+     */
     public ValidatableResponse update(CommentDto commentDto, int id, HttpStatus status) {
         LOGGER.info("Update Comment by id [{}]", id);
         return put(
-            this.specification,
-            COMMENTS_RESOURCE_END,
-            commentDto,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_RESOURCE_END,
+                commentDto,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves the comment with the specified ID.
+     *
+     * @param id the ID of the comment to retrieve
+     * @return the CommentDto object representing the retrieved comment
+     * @see CommentDto
+     */
     public CommentDto getById(int id) {
         return getById(id, HttpStatus.OK)
-            .extract().as(CommentDto.class);
+                .extract().as(CommentDto.class);
     }
 
+    /**
+     * Retrieves the comment with the specified ID and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param id     the ID of the comment to retrieve
+     * @param status the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * CommentEndpoint commentEndpoint = new CommentEndpoint(requestSpecification);
+     * CommentDto comment = commentEndpoint.getById(1);
+     * System.out.println("Comment Content: " + comment.getContent());
+     * }</pre>
+     */
     public ValidatableResponse getById(int id, HttpStatus status) {
         LOGGER.info("Get Comment by id [{}]", id);
         return get(
-            this.specification,
-            COMMENTS_RESOURCE_END,
-            String.valueOf(id))
-            .statusCode(status.getCode());
+                this.specification,
+                COMMENTS_RESOURCE_END,
+                String.valueOf(id))
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves all comments.
+     *
+     * @return a List of CommentDto objects representing all comments
+     * @see CommentDto
+     */
     public List<CommentDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(CommentDto[].class));
     }
 
+    /**
+     * Retrieves all comments and validates the response against the specified HttpStatus.
+     *
+     * @param status the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * CommentEndpoint commentEndpoint = new CommentEndpoint(requestSpecification);
+     * List<CommentDto> comments = commentEndpoint.getAll();
+     * for (CommentDto comment : comments) {
+     *     System.out.println("Comment ID: " + comment.getId() + ", Content: " + comment.getContent());
+     * }
+     * }</pre>
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Comments");
         ValidatableResponse response = get(this.specification, COMMENTS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
-
 }
--- UserEndpoint.java
+++ UserEndpoint.java
@@ -1,3 +1,14 @@
+/**
+ * Represents a REST API endpoint for managing user resources.
+ * This class extends the AbstractWebEndpoint class.
+ *
+ * <p>
+ * The UserEndpoint class provides methods for creating, updating, and retrieving user information
+ * via the REST API. It inherits common functionality from the AbstractWebEndpoint class.
+ * </p>
+ *
+ * @see AbstractWebEndpoint
+ */
 package com.softserve.taf.services.placeholder.endpoints;
 
 import io.restassured.response.ValidatableResponse;
@@ -15,62 +26,164 @@ public class UserEndpoint extends AbstractWebEndpoint {
     private static final String USERS_END = "/users";
     private static final String USERS_RESOURCE_END = "/users/{userID}";
 
+    /**
+     * Constructs a new UserEndpoint with the given RequestSpecification.
+     *
+     * @param specification the RequestSpecification used for API requests
+     * @see RequestSpecification
+     */
     public UserEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+     * Creates a new user using the provided UserDto object.
+     *
+     * @param userDto the UserDto object representing the user to be created
+     * @return the created UserDto object
+     * @see UserDto
+     */
     public UserDto create(UserDto userDto) {
         return create(userDto, HttpStatus.CREATED)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Creates a new user using the provided UserDto object and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param userDto the UserDto object representing the user to be created
+     * @param status  the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see UserDto
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * UserDto user = new UserDto("John Doe", "john.doe@example.com");
+     * UserEndpoint userEndpoint = new UserEndpoint(requestSpecification);
+     * UserDto createdUser = userEndpoint.create(user);
+     * System.out.println("Created User ID: " + createdUser.getId());
+     * }</pre>
+     */
     public ValidatableResponse create(UserDto userDto, HttpStatus status) {
         LOGGER.info("Create new User");
         return post(
-            this.specification,
-            USERS_END,
-            userDto)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_END,
+                userDto)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Updates an existing user with the provided UserDto object and user ID.
+     *
+     * @param id      the ID of the user to be updated
+     * @param userDto the UserDto object representing the updated user
+     * @return the updated UserDto object
+     * @see UserDto
+     */
     public UserDto update(int id, UserDto userDto) {
         return update(userDto, id, HttpStatus.OK)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Updates an existing user with the provided UserDto object, user ID, and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param userDto the UserDto object representing the updated user
+     * @param id      the ID of the user to be updated
+     * @param status  the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see UserDto
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * UserDto user = new UserDto("John Doe", "john.doe@example.com");
+     * UserEndpoint userEndpoint = new UserEndpoint(requestSpecification);
+     * UserDto updatedUser = userEndpoint.update(1, user);
+     * System.out.println("Updated User: " + updatedUser.getName());
+     * }</pre>
+     */
     public ValidatableResponse update(UserDto userDto, int id, HttpStatus status) {
         LOGGER.info("Update User by id [{}]", id);
         return put(
-            this.specification,
-            USERS_RESOURCE_END,
-            userDto,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_RESOURCE_END,
+                userDto,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves the user with the specified ID.
+     *
+     * @param id the ID of the user to retrieve
+     * @return the UserDto object representing the retrieved user
+     * @see UserDto
+     */
     public UserDto getById(String id) {
         return getById(id, HttpStatus.OK)
-            .extract().as(UserDto.class);
+                .extract().as(UserDto.class);
     }
 
+    /**
+     * Retrieves the user with the specified ID and validates the response
+     * against the specified HttpStatus.
+     *
+     * @param id     the ID of the user to retrieve
+     * @param status the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * UserEndpoint userEndpoint = new UserEndpoint(requestSpecification);
+     * UserDto user = userEndpoint.getById("1");
+     * System.out.println("User Name: " + user.getName());
+     * }</pre>
+     */
     public ValidatableResponse getById(String id, HttpStatus status) {
         LOGGER.info("Get User by id [{}]", id);
         return get(
-            this.specification,
-            USERS_RESOURCE_END,
-            id)
-            .statusCode(status.getCode());
+                this.specification,
+                USERS_RESOURCE_END,
+                id)
+                .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves all users.
+     *
+     * @return a List of UserDto objects representing all users
+     * @see UserDto
+     */
     public List<UserDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(UserDto[].class));
     }
 
+    /**
+     * Retrieves all users and validates the response against the specified HttpStatus.
+     *
+     * @param status the expected HttpStatus of the response
+     * @return the ValidatableResponse object representing the API response
+     * @see HttpStatus
+     * @see ValidatableResponse
+     * @example
+     * <pre>{@code
+     * UserEndpoint userEndpoint = new UserEndpoint(requestSpecification);
+     * List<UserDto> users = userEndpoint.getAll();
+     * for (UserDto user : users) {
+     *     System.out.println("User ID: " + user.getId() + ", Name: " + user.getName());
+     * }
+     * }</pre>
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Users");
         ValidatableResponse response = get(this.specification, USERS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
 }
